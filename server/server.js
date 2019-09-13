890edb95e0756987a42132734f0a9f3fda9844f6
Jesus <horus_hades@hotmail.com>
2019-09-13 17:53:29 +0200

    feat(server): handle sessions

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import express from "express";
+import uuid from "uuid/v4";
+import expressSession from "express-session";
+import sessionFileStore from "session-file-store";
+
+const FileStore = sessionFileStore(expressSession);
 
 export class CalendariusServer {
   constructor() {
     this.createApp();
     this.config();
+    this.initSession();
+    this.defineRoutes();
     this.listen();
   }
 
@@ -11,8 +18,33 @@ export class CalendariusServer {
     this._app = express();
   }
 
+  initSession() {
+    // add & configure middleware
+    this._app.use(
+      expressSession({
+        genid: req => {
+          console.log("Inside the session middleware");
+          console.log(req.sessionID);
+          return uuid(); // use UUIDs for session IDs
+        },
+        store: new FileStore(),
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true
+      })
+    );
+  }
+
   config() {
     this._port = process.env.PORT || 8080;
+  }
+
+  defineRoutes() {
+    this._app.get("/", (req, res) => {
+      console.log("Inside the homepage callback function");
+      console.log(req.sessionID);
+      res.send(`you just hit the home page\n`);
+    });
   }
 
   listen() {
