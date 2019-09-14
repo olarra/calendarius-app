8476d94164a48908dcfd5b501d556b3e26a58637
Jesus <horus_hades@hotmail.com>
2019-09-14 13:50:42 +0200

    feat(nomatch): page prototyping

--- client/src/ui/pages/NoMatch.js
+++ client/src/ui/pages/NoMatch.js
@@ -1,3 +1,31 @@
 import React from "react";
+import { Container, Row, Col, Image, Form, Alert, Button } from "react-bootstrap";
 
-export const NoMatch = () => <h1>NoMatch</h1>;
+export const NoMatch = () => <Container fluid={true}>
+<Row>
+  <Col style={styles.presentation}>
+    <p style={styles.error}>404</p>
+    <p style={styles.subtitle}>Ups! Page not Found.</p>
+  </Col>
+</Row>
+</Container>;
+
+const styles = {
+  presentation: {
+    height: "100vh",
+    alignItems:"center",
+    justifyContent:"center",
+    display:"flex",
+    flexDirection:"column",
+    background: "linear-gradient(to bottom, #ec6f66, #f3a183)"
+  },
+  error :{
+    color:"#ffff",
+    fontSize:120
+  },
+  subtitle :{
+    color:"#ffff",
+    fontSize:80,
+    fontWeight:100
+  }
+}
