a0092f1d2256c2b6cdf31a4b1e47928896b4d446
Jesus GARNICA OLARRA <jesus.garnica.olarra@accenture.com>
2019-09-17 09:40:14 +0200

    feat(redux): subscribe to state changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css"; // react-bootsrap dependency
 import "./index.css";
 import { CalendariousRouter } from "./navigation";
 
-import { configStore } from "./redux";
+import { getStore } from "./redux";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <Provider store={configStore()}>
+  <Provider store={getStore()}>
     <CalendariousRouter />
   </Provider>,
   document.getElementById("root")
--- client/src/redux/configStore.js
+++ client/src/redux/configStore.js
@@ -2,5 +2,28 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import ReduxThunk from "redux-thunk"; //async actions
 
-export const configStore = () =>
-  createStore(rootReducer, {}, applyMiddleware(ReduxThunk));
+const selectAgenda = (state) => state.agenda;
+let currentValue
+
+const handleChange = (store) => {
+  let previousValue = currentValue
+  currentValue = selectAgenda(store.getState())
+
+  if (previousValue !== currentValue) {
+    console.log(
+      'Some deep nested property changed from',
+      previousValue,
+      'to',
+      currentValue
+    )
+  }
+}
+
+export const buildSotore = () => {
+  const store = createStore(rootReducer, {}, applyMiddleware(ReduxThunk));
+  store.subscribe(()=>handleChange(store))
+  return store;
+}
+
+
+export const getStore = () => buildSotore();
